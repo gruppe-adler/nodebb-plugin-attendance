23ef0aa97873502915942ba2b41de746e0d076b1
Moritz Schmidt <fusselwurm@gmail.com>
2017-01-08 22:19:52 +0100

    more of the cleanuppings

--- registerApi.js
+++ registerApi.js
@@ -30,20 +30,29 @@ var ensureFloatType = function (type) {
     return stringTypeFloatMap[type];
 };
 
-var getZsetKey = function (topic_id, type) {
-    return 'tid:%s:attendance:%s'.replace('%s', topic_id).replace('%s', type);
-};
-
-var getAsyncAttendancesDeleter = function (tid, uid, type) {
-    return function (next) {
-        db.sortedSetRemove(getZsetKey(tid, type), uid, next);
-    };
-};
-
 var getUserAttendance = function (attendants, uid) {
     return attendants.filter(function (a) {
         return a.uid == uid;
     }).pop();
+};
+
+var getUsersWithFields = function (currentUser, attendants, next) {
+    users.getUsersWithFields(
+        attendants.map(function (attendant) {
+            return attendant.uid;
+        }),
+        ['uid', 'username', 'userslug', 'picture', 'icon:bgColor', 'icon:text'],
+        currentUser,
+        function (err, results) {
+            if (err) {return next(err); }
+
+            users = {};
+            results.forEach(function (user) {
+                users[user.uid] = user;
+            });
+            next(null, users);
+        }
+    );
 };
 
 var async = require('async');
@@ -61,45 +70,22 @@ module.exports = function (params, callback) {
 
     router.post('/api/attendance/:tid',
         function (req, res, next) {
-            var type = req.body.type;
             var probability = req.body.probability;
             var tid = req.params.tid;
             var uid = req.uid;
-            var stringType = floatTypeStringMap[type] || type;
-            var timestamp = (new Date()).getTime();
 
             try {
-                ensureFloatType(type);
+                probability = ensureFloatType(req.body.type || probability);
             } catch (e) {
                 return res.status(400).json({"error": e.message});
             }
-
-            async.parallel(
-                types.map(function (type) {
-                    return getAsyncAttendancesDeleter(tid, uid, type);
-                }),
-                function (err, results) {
-                    if (err) {
-                        return res.status(500).json({error: err});
-                    }
-                    db.sortedSetAdd(getZsetKey(tid, stringType), timestamp, uid, function (err, data) {
-                        if (err) {
-                            return next(res.status(500).json({error: err}));
-                        }
-                        res.status(200).json({
-                            added: type,
-                            user: uid
-                        });
-                    });
-                }
-            );
 
             floatPersistence.set(
                 tid,
                 {
                     uid: uid,
-                    probability: probability || ensureFloatType(type),
-                    timestamp: timestamp
+                    probability: probability,
+                    timestamp: (new Date()).getTime()
                 },
                 function (err, result) {
                     if (err) {
@@ -123,24 +109,21 @@ module.exports = function (params, callback) {
             }
 
             var attendants = results;
-            var userIds = attendants.map(function (attendant) {
-                return attendant.uid;
-            });
 
-            users.getUsersWithFields(userIds, ['uid', 'username', 'userslug', 'picture', 'icon:bgColor', 'icon:text'], currentUser, function (err, users) {
+            getUsersWithFields(currentUser, attendants, function (err, users) {
                 if (err) {
                     return res.status(500).json({err: err});
                 }
 
                 attendants.forEach(function (attendant) {
-                    var u = users.filter(function (user) {
-                        return user.uid == attendant.uid;
-                    }).pop();
-
+                    var u = users[attendant];
+                    if (!u) {
+                        return winston.error('unknown user with id ' + attendant.uid);
+                    }
                     _(attendant).extend(_(u).pick(['username', 'userslug', 'picture', 'icon:bgColor', 'icon:text']));
                 });
 
-                res.status(200).json({
+                return res.status(200).json({
                     myAttendance: getUserAttendance(attendants, currentUser),
                     attendants: attendants
                 });
