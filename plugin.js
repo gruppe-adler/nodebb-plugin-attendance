d7baba5a35325160b5cfb49a53c2baafb4dbf2c1
Moritz Schmidt <fusselwurm@gmail.com>
2017-07-27 20:40:22 +0200

    merge color-by-date; limit sorting to event/categories

--- plugin.js
+++ plugin.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const meta = require('./plugin.json');
+const _ = require('underscore');
 meta.nbbId = meta.id.replace(/nodebb-plugin-/, '');
 
 function noop() {}
@@ -28,4 +29,29 @@ module.exports.admin = {
 
         callback(null, custom_header);
     }
+};
+
+const titleToTimestamp = _.memoize(function (title) {
+    const matches = title.match(/([0-9]{4}-[0-9]{2}-[0-9]{2})[^a-z0-9]/i);
+    if (!matches) {
+        return 0;
+    }
+    return parseInt((new Date(matches[1])).getTime() / 1000, 10);
+});
+
+module.exports.colorTopic = function (data, cb) {
+
+    data.topics.forEach(function (topic) {
+        topic.referencetime = titleToTimestamp(topic.title) || topic.lastposttime;
+    });
+    data.topics = data.topics.sort(function (a, b) {
+        if (b.pinned && !a.pinned) {
+            return 1;
+        } else if (!b.pinned && a.pinned) {
+            return -1;
+        }
+        return b.referencetime - a.referencetime;
+    });
+
+    cb(null, data);
 };
---
+++ public/colorTopic.js
@@ -0,0 +1,76 @@
+/*global $ */
+(function colorTopic() {
+
+    /**
+     * taken from underscoreJs
+     */
+    var _ = {
+        now: Date.now || function() {
+            return new Date().getTime();
+        },
+        debounce: function(func, wait, immediate) {
+            var timeout, args, context, timestamp, result;
+
+            var later = function() {
+                var last = _.now() - timestamp;
+
+                if (last < wait && last >= 0) {
+                    timeout = setTimeout(later, wait - last);
+                } else {
+                    timeout = null;
+                    if (!immediate) {
+                        result = func.apply(context, args);
+                        if (!timeout) context = args = null;
+                    }
+                }
+            };
+
+            return function() {
+                context = this;
+                args = arguments;
+                timestamp = _.now();
+                var callNow = immediate && !timeout;
+                if (!timeout) timeout = setTimeout(later, wait);
+                if (callNow) {
+                    result = func.apply(context, args);
+                    context = args = null;
+                }
+
+                return result;
+            };
+        }
+    };
+
+    var titleToTimestamp = function (title) {
+        var matches = title.trim().match(/([0-9]{4}-[0-9]{2}-[0-9]{2})([^0-9a-z])/i);
+        if (!matches) {
+            return 0;
+        }
+        return parseInt((new Date(matches[1])).getTime() / 1000, 10);
+    };
+
+    var refresh = _.debounce(function () {
+        var now = parseInt((new Date()).getTime() / 1000, 10);
+        var topicRows = document.querySelectorAll('[component="category/topic"]');
+        Array.prototype.forEach.call(topicRows, function (categoryItem) {
+            var meta = categoryItem.querySelector('[component="topic/header"] a'); 
+            var topicTime = titleToTimestamp(meta.getAttribute('content') || meta.textContent || '');
+            var dataRelativeTime = '';
+            var timeDiff = now - topicTime;
+
+            if (topicTime === 0) {
+                dataRelativeTime = 'now';
+            } else if (timeDiff < 86400) {
+                dataRelativeTime = 'future';
+            } else if (timeDiff > 86400) {
+                dataRelativeTime = 'past'
+            }
+
+            categoryItem.setAttribute('data-relative-time', dataRelativeTime);
+        });
+    }, 300);
+
+    $(window).bind('action:ajaxify.contentLoaded', refresh);
+    $(window).bind('action:topics.loaded', refresh);
+    $(window).bind('action:categories.loaded', refresh);
+}());
